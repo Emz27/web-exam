34648a886f79c6d0980c4f7795e57a4ec2a5cb37
Emz27 <emmanuelbenitez1996@gmail.com>
2017-12-31 16:30:17 +0800

    .

--- src/api/session.php
+++ src/api/session.php
@@ -3,7 +3,8 @@
       session_start();
   }
   $data['isLogged'] = false;
-
+  if (isset($_SESSION['id']) && $_SESSION['isLogged'] == true)
+  {
       $data['isLogged'] = true;
       $data['id'] = $_SESSION['id'];
       $data['firstname'] = $_SESSION['firstname'];
@@ -11,6 +12,6 @@
       $data['lastname'] = $_SESSION['lastname'];
       $data['type'] = $_SESSION['type'];
       $data['date_created'] = $_SESSION['date_created'];
-
+  }
   echo json_encode($data);
 ?>
