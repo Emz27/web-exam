0e50d443addefb6829aac8271ba8a402e6ef1282
Emz27 <emmanuelbenitez1996@gmail.com>
2017-12-31 16:08:46 +0800

    Pages

---
+++ src/app/LoginPage.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+import 'whatwg-fetch'
+
+class LoginPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = {
+      username: '',
+      password: '',
+      isCorrect: true,
+    }
+  }
+  handleUsernameChange(event){
+    this.setState({username : event.target.value})
+  }
+  handlePasswordChange(event){
+    this.setState({password : event.target.value})
+  }
+  handleLogin(event){
+    var user = this;
+
+    $.ajax({
+      url: "../api/login.php",
+      method: "GET",
+      data: {
+        username: user.state.username,
+        password: user.state.password
+      }
+    })
+    .done(function(data){
+      data = JSON.parse(data);
+      console.log(data);
+      console.log(data.isLogged)
+      if(data.isLogged){
+        console.log("im logged!")
+        user.props.changeUser(data);
+      }
+      else {
+        user.setState({
+          isCorrect: false
+        })
+      }
+    })
+    .fail(function(xhr) {
+        console.dir(xhr);
+        return alert("error in fetching session: "+ xhr);
+    });
+    event.preventDefault();
+  }
+  render(){
+    return (
+      <div>
+      <form onSubmit={this.handleLogin}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handlePasswordChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
+    )
+  }
+}
+
+
+export default LoginPage
