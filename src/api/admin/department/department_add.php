239f1dfa2942ee399f3f039060934962bbe5464b
Emz27 <emmanuelbenitez1996@gmail.com>
2018-01-02 04:46:41 +0800

    department php api

---
+++ src/api/admin/department/department_add.php
@@ -0,0 +1,21 @@
+<?php
+  include("../../config.php");
+  session_start();
+  $description = isset($_GET['description'])?$_GET['description']:"";
+
+
+  $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO `department`( `description`)
+  VALUES ('$description')";
+  $conn->query($sql);
+
+  echo $conn->error;
+
+  $conn->close();
+
+?>
